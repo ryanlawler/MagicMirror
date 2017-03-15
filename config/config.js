c2f06cb898d511eae5ffe2dfba1fc8f7595f4156
ryanlawler <ryanlawler@berkeley.edu>
2017-03-14 23:45:21 -0700

    Deleted notification top bar thing

--- config/config.js
+++ config/config.js
@@ -16,10 +16,6 @@ var config = {
             module: 'alert',
         },
         {
-            module: "updatenotification",
-            position: "top_bar"
-        },
-        {
             module: 'clock',
             position: 'top_left'
         },

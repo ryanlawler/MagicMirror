4d94d0aa5d79e5838d8a6a7b135f0674791ea634
ryanlawler <ryanlawler@berkeley.edu>
2017-08-19 17:33:45 -0700

    Changed city back to aliso

--- config/config.js
+++ config/config.js
@@ -15,13 +15,13 @@ var config = {
             module: 'alert',
         },
         {
-            module: 'clock',
-            position: 'top_left'
+            module: "clock",
+            position: "top_left"
         },
         {
-            module: 'calendar',
-            header: 'US Holidays',
-            position: 'top_left',
+            module: "calendar",
+            header: "Courtney's Events",
+            position: "top_left",
             config: {
                 calendars: [{
                         symbol: 'calendar-check-o ',
@@ -30,10 +30,6 @@ var config = {
                     {
                         symbol: "calendar",
                         url: "https://calendar.google.com/calendar/ical/courtneylawler11%40gmail.com/private-489770cacebb966d3a1d13f79f2cdcf6/basic.ics",
-                    },
-                    {
-                        symbol: "calendar-o",
-                        url: "https://outlook.office365.com/owa/calendar/b4508fba927b49e7b38fc821a4ef79fd@yum.com/46e1a5e8f2e64dcd9e2165273d53faa616049492060532477113/calendar.ics",
                     }
                 ]
             }
@@ -46,8 +42,8 @@ var config = {
             module: 'currentweather',
             position: 'top_right',
             config: {
-                location: 'Berkeley',
-                locationID: '5327684', //ID from http://www.openweathermap.org
+                location: 'Aliso Viejo',
+                locationID: '5323163', //ID from http://www.openweathermap.org
                 appid: 'fb48fecfe81fedbf19d33b77871698c9'
             }
         },
@@ -56,8 +52,8 @@ var config = {
             position: 'top_right',
             header: 'Weather Forecast',
             config: {
-                location: 'Berkeley',
-                locationID: '5327684', //ID from http://www.openweathermap.org
+                location: 'Aliso Viejo',
+                locationID: '5323163', //ID from http://www.openweathermap.org
                 appid: 'fb48fecfe81fedbf19d33b77871698c9'
             }
         },
@@ -78,4 +74,4 @@ var config = {
 }
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== 'undefined') { module.exports = config; }
+if (typeof module !== "undefined") { module.exports = config; }

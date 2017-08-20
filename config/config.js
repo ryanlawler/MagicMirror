4401d457cc5eed472557c8e5c72a74a6168b393c
ryanlawler <ryanlawler@berkeley.edu>
2017-08-19 17:27:00 -0700

    fixed config

--- config/config.js
+++ config/config.js
@@ -7,101 +7,75 @@
 var config = {
     port: 8080,
 
-    language: 'en',
+    language: "en",
     timeFormat: 12,
-    units: 'imperial',
+    units: "imperial",
 
     modules: [{
-                module: 'alert',
-            },
-            {
-                module: 'clock',
-                position: 'top_left'
-            },
-            {
-                module: 'calendar',
-                header: 'US Holidays',
-                position: 'top_left',
-                config: {
-                    calendars: [{
-                            symbol: 'calendar-check-o ',
-                            url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
-                        },
-                        {
-                            symbol: "calendar",
-                            url: "https://calendar.google.com/calendar/ical/courtneylawler11%40gmail.com/private-489770cacebb966d3a1d13f79f2cdcf6/basic.ics",
-                        },
-                        {
-                            symbol: "calendar-o",
-                            url: "https://outlook.office365.com/owa/calendar/b4508fba927b49e7b38fc821a4ef79fd@yum.com/46e1a5e8f2e64dcd9e2165273d53faa616049492060532477113/calendar.ics",
-                        },
-                    ]
-                }
-            },
-            {
-                module: 'compliments',
-                position: 'lower_third'
-            },
-            {
-                module: 'currentweather',
-                position: 'top_right',
-                config: {
-                    location: 'Berkeley',
-                    locationID: '5327684', //ID from http://www.openweathermap.org
-                    appid: 'fb48fecfe81fedbf19d33b77871698c9'
-                }
-            },
-            {
-                module: 'weatherforecast',
-                position: 'top_right',
-                header: 'Weather Forecast',
-                config: {
-                    location: 'Berkeley',
-                    locationID: '5327684', //ID from http://www.openweathermap.org
-                    appid: 'fb48fecfe81fedbf19d33b77871698c9'
-                }
-            },
-            {
-                module: 'newsfeed',
-                position: 'bottom_bar',
-                config: {
-                    feeds: [{
-                        title: "New York Times",
-                        url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-                    }],
-                    showSourceTitle: true,
-                    showPublishDate: true
-                } <<
-                <<
-                <<
-                < HEAD
-            },
-            {
-                module: 'imageViewer',
-                position: 'bottom_left',
-            },
-            {
-                module: 'imageViewer2',
-                position: 'bottom_right',
+            module: 'alert',
+        },
+        {
+            module: 'clock',
+            position: 'top_left'
+        },
+        {
+            module: 'calendar',
+            header: 'US Holidays',
+            position: 'top_left',
+            config: {
+                calendars: [{
+                        symbol: 'calendar-check-o ',
+                        url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
+                    },
+                    {
+                        symbol: "calendar",
+                        url: "https://calendar.google.com/calendar/ical/courtneylawler11%40gmail.com/private-489770cacebb966d3a1d13f79f2cdcf6/basic.ics",
+                    },
+                    {
+                        symbol: "calendar-o",
+                        url: "https://outlook.office365.com/owa/calendar/b4508fba927b49e7b38fc821a4ef79fd@yum.com/46e1a5e8f2e64dcd9e2165273d53faa616049492060532477113/calendar.ics",
+                    }
+                ]
             }
-        ] ===
-        ===
-        = >>>
-        >>>
-        >
-        9739e46 b392b9235d010ffca70c9eb3120eedaa2
+        },
+        {
+            module: 'compliments',
+            position: 'lower_third'
+        },
+        {
+            module: 'currentweather',
+            position: 'top_right',
+            config: {
+                location: 'Berkeley',
+                locationID: '5327684', //ID from http://www.openweathermap.org
+                appid: 'fb48fecfe81fedbf19d33b77871698c9'
+            }
+        },
+        {
+            module: 'weatherforecast',
+            position: 'top_right',
+            header: 'Weather Forecast',
+            config: {
+                location: 'Berkeley',
+                locationID: '5327684', //ID from http://www.openweathermap.org
+                appid: 'fb48fecfe81fedbf19d33b77871698c9'
+            }
+        },
+        {
+            module: 'newsfeed',
+            position: 'bottom_bar',
+            config: {
+                feeds: [{
+                    title: "New York Times",
+                    url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+                }],
+                showSourceTitle: true,
+                showPublishDate: true
+            }
 
-};
+        }
+    ]
+}
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-<<
-<<
-<<
-< HEAD
-if (typeof module !== 'undefined') { module.exports = config; } ===
-===
-=
-if (typeof module !== 'undefined') { module.exports = config; } >>>
->>>
->
-9739e46 b392b9235d010ffca70c9eb3120eedaa2
+if (typeof module !== 'undefined') { module.exports = config; }
